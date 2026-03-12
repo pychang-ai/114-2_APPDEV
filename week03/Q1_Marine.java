c4b5a1fa352afe7f987d95911eff124d45e81963
xian1022 <c112181113@nkust.edu.tw>
2026-03-12 11:31:55 +0800

    new

--- week03/Q1_Marine.java
+++ week03/Q1_Marine.java
@@ -1,5 +1,5 @@
 public class Q1_Marine {
-public static void main(String[] args) {
-System.out.println("Hello, Ocean!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, Ocean!");
+    }
 }
