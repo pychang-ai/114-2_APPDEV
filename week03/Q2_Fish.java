85b35821ba0934bfb39a8d43da75db6fffcb7441
EK181121 <c113181121@nkust.edu.tw>
2026-03-26 11:30:32 +0800

    add sm

--- week03/Q2_Fish.java
+++ week03/Q2_Fish.java
@@ -1,13 +1,13 @@
-public class Q2_fish{
+public class Q2_Fish{
     String name;
     double weight;
 
     public void displayinf(){
-         System.out.println("This fish's name is:" + name + "Weight: "+ wieght +"KG");
+         System.out.println("This fish's name is:" + name + "Weight: "+ weight +"KG");
     }
 
     public static void main(String [] args){
-        Q2_fish myfish = new Q2_fish();
+        Q2_Fish myfish = new Q2_Fish();
         myfish.name = "Black fish";
         myfish.weight =250.5;
 
